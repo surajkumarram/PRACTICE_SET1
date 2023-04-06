ee6f1159d21b198b4bd36a8082998212adfdea78
Suraj kumar <101385745+surajkumarram@users.noreply.github.com>
2023-04-06 12:27:36 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRACTICE_SET1.MvcApplication" Language="C#" %>
---
+++ Models/Modelset1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PRACTICE_SET1.Models
+{
+    public class Modelset1
+    {
+    }
+}
